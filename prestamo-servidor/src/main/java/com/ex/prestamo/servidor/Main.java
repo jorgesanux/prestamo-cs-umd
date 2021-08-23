6ee65cbf1d7715e5db0b67014bd16086cb6321b4
Jorge Sanabria <jorgesanux1@gmail.com>
2021-08-22 19:53:39 -0500

    Creadop prestamo servidor

---
+++ prestamo-servidor/src/main/java/com/ex/prestamo/servidor/Main.java
@@ -0,0 +1,9 @@
+package com.ex.prestamo.servidor;
+
+import com.ex.prestamo.servidor.view.MainForm;
+
+public class Main {
+    public static void main(String[] args){
+        MainForm.open();
+    }
+}
