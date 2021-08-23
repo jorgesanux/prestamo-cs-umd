2c4af3516b3915c08d7b57eb7f0e03e21a5fdb7c
Jorge Sanabria <jorgesanux1@gmail.com>
2021-08-22 21:54:57 -0500

    Agregadas clases de Prestamo y EstudioPrestamo

---
+++ prestamo-cliente/src/main/java/com/ex/prestamo/cliente/model/EstudioPrestamo.java
@@ -0,0 +1,58 @@
+package com.ex.prestamo.cliente.model;
+
+public class EstudioPrestamo {
+    private Prestamo prestamo;
+    private double valorCuota;
+    private double valorInteresMensual;
+    private double totalPagar;
+
+    public EstudioPrestamo(Prestamo prestamo, double valorCuota, double valorInteresMensual, double totalPagar) {
+        this.prestamo = prestamo;
+        this.valorCuota = valorCuota;
+        this.valorInteresMensual = valorInteresMensual;
+        this.totalPagar = totalPagar;
+    }
+
+    public Prestamo getPrestamo() {
+        return prestamo;
+    }
+
+    public void setPrestamo(Prestamo prestamo) {
+        this.prestamo = prestamo;
+    }
+
+    public double getValorCuota() {
+        return valorCuota;
+    }
+
+    public void setValorCuota(double valorCuota) {
+        this.valorCuota = valorCuota;
+    }
+
+    public double getValorInteresMensual() {
+        return valorInteresMensual;
+    }
+
+    public void setValorInteresMensual(double valorInteresMensual) {
+        this.valorInteresMensual = valorInteresMensual;
+    }
+
+    public double getTotalPagar() {
+        return totalPagar;
+    }
+
+    public void setTotalPagar(double totalPagar) {
+        this.totalPagar = totalPagar;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("EstudioPrestamo{prestamo=").append(prestamo);
+        sb.append(", valorCuota=").append(valorCuota);
+        sb.append(", valorInteresMensual=").append(valorInteresMensual);
+        sb.append(", totalPagar=").append(totalPagar);
+        sb.append('}');
+        return sb.toString();
+    }
+}
